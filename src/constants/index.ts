487e198a40ddd8cb14eecf125b95290b581562aa
thomga <gabriel.thom02@gmail.com>
2023-05-26 17:07:51 -0300

    chore: wip

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,9 +10,13 @@ const WETH_ADDRESS = '0x4200000000000000000000000000000000000006'
 
 export const WETH_ADDRESSES = [WETH_ADDRESS]
 
-export const TOKEN_HIDE: { [key: string]: string[] } = {}
+export const TOKEN_HIDE: { [key: string]: string[] } = {
+  [SupportedNetwork.ROLLUX]: ['0x0000000000000000000000000000000000000001'],
+}
 
-export const POOL_HIDE: { [key: string]: string[] } = {}
+export const POOL_HIDE: { [key: string]: string[] } = {
+  [SupportedNetwork.ROLLUX]: ['0x0000000000000000000000000000000000000001'],
+}
 
 export const START_BLOCKS: { [key: string]: number } = {
   [SupportedNetwork.ROLLUX]: 215160,
