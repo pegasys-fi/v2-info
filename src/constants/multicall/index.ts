ca74d423b273d6e40690884f2845d3812647db70
David Potolski Lafetá <davidpotolskilafeta@gmail.com>
2023-06-28 17:09:59 -0300

    fix: updates MULTICALL_NETWORKS address

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { SupportedChainId } from 'constants/chains'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId: number]: string } = {
-  [SupportedChainId.ROLLUX]: '0x0Cb0a51572A2B0c97ba2dFf061fB6bFed662fD40',
+  [SupportedChainId.ROLLUX]: '0x6aE73f43cc18Bb9ffB35204023C6C7897CA879C4',
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
